fca0a7a5994cbe882fcba217c5057592d9cc24dc
Vedika Udgir <vedikaudgir@gmail.com>
2025-09-14 22:00:15 +0530

    changedTheFolderAndCreatedANewRepo

---
+++ Models/Entities/Playlists.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MoodPlaylistManager.Models.Entities
+{
+    public class Playlists
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string PlaylistName { get; set; } = string.Empty;
+        public string Mood { get; set; } = string.Empty;
+        public string Artist { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public ICollection<Songs> Songs { get; set; } = new List<Songs>();
+    }
+}
